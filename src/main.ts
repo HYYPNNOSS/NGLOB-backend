1e1ec692226e1bab7ef15ef0bc11df0bc6a9ada6
HYYPNNOSS <azert9412@gmail.com>
2026-06-12 07:41:31 +0100

    d

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,9 @@ async function bootstrap() {
   // CORS
   app.enableCors({
     origin: process.env.FRONTEND_URL 
-      ? [process.env.FRONTEND_URL, 'http://localhost:3000'] 
-      : 'http://localhost:3000',
+      ? [process.env.FRONTEND_URL, 'https://ngob.vercel.app', 'http://localhost:3000'] 
+      : ['https://ngob.vercel.app', 'http://localhost:3000'], // Fallback if FRONTEND_URL isn't set
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
  
